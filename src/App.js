d207686bce919391877e607eb18d6ab5c4614442
maazkh10 <120632580+maazkh10@users.noreply.github.com>
2023-08-02 10:12:29 +0530

    A new code added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,109 @@
-import logo from './logo.svg';
-import './App.css';
+import dayjs from "dayjs";
+import './App.css'
+import React, { useState } from "react";
+import { generateDate, months } from "../src/utils/calender";
+import cn from "../src/utils/cn";
+import { GrFormNext, GrFormPrevious } from "react-icons/gr";
+import TodoForm from "./component/TodoForm";
+import TodoList from "./component/TodoList";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function Calendar() {
+	const days = ["S", "M", "T", "W", "T", "F", "S"];
+	const currentDate = dayjs();
+	const [today, setToday] = useState(currentDate);
+	const [selectDate, setSelectDate, ] = useState(currentDate) ;
+	return (
+		<div className="flex gap-10 sm:divide-x justify-center sm:w-1/2 mx-auto  h-screen items-center sm:flex-row flex-col">
+			<div className="w-96 h-96 ">
+				<div className="flex justify-between items-center">
+					<h1 className="select-none font-semibold">
+						{months[today.month()]}, {today.year()}
+					</h1>
+					<div className="flex gap-10 items-center ">
+						<GrFormPrevious
+							className="w-5 h-5 cursor-pointer hover:scale-105 transition-all"
+							onClick={() => {
+								setToday(today.month(today.month() - 1));
+							}}
+						/>
+						<h1
+							className=" cursor-pointer hover:scale-105 transition-all"
+							onClick={() => {
+								setToday(currentDate);
+							}}
+						>
+							Today
+						</h1>
+						<GrFormNext
+							className="w-5 h-5 cursor-pointer hover:scale-105 transition-all"
+							onClick={() => {
+								setToday(today.month(today.month() + 1));
+							}}
+						/>
+					</div>
+				</div>
+				<div className="grid grid-cols-7 ">
+					{days.map((day, index) => {
+						return (
+							<h1
+								key={index}
+								className="text-sm text-center h-14 w-14 grid place-content-center text-gray-500 select-none"
+							>
+								{day}
+							</h1>
+						);
+					})}
+				</div>
+
+				<div className=" grid grid-cols-7 ">
+					{generateDate(today.month(), today.year()).map(
+						({ date, currentMonth, today }, index) => {
+							return (
+								<div
+									key={index}
+									className="p-2 text-center h-14 grid place-content-center text-sm border-t"
+								>
+									<h1
+										className={cn(
+											currentMonth ? "" : "text-gray-400",
+											today
+												? "bg-red-600 text-white"
+												: "",
+											selectDate
+												.toDate()
+												.toDateString() ===
+												date.toDate().toDateString()
+												? "bg-black text-white"
+												: "",
+											"h-10 w-10 rounded-full grid place-content-center hover:bg-black hover:text-white transition-all cursor-pointer select-none"
+										)}
+										onClick={() => {
+											setSelectDate(date);
+										}}
+									>
+										{date.date()}
+									</h1>
+								</div>
+							);
+						}
+					)}
+				</div>
+			</div>
+			<div className="h-96 w-96 sm:px-5" style={{ padding: '50px' }}>
+  <div>
+    <h1 className="font-semibold">
+      Schedule for {selectDate.toDate().toDateString()}
+    </h1>
+  </div>
+  <div style={{ marginTop: '0px' }}>
+    
+	
+  </div>
+  <TodoList />
+</div>
+
+			
+			
+		</div>
+	);
 }
-
-export default App;
